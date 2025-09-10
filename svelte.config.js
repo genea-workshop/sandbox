b2bf150f75a0573607663de00e42296997a9535f
Mihail <tsakovm@gmail.com>
2025-09-10 13:32:56 +0200

    attempt to fix build

--- svelte.config.js
+++ svelte.config.js
@@ -7,12 +7,7 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { 
-		adapter: adapter(),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/sandbox' : ''
-		}
-	 },
+	kit: {adapter: adapter()},
 	extensions: ['.svelte', '.svx']
 };
 
